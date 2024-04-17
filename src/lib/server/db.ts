67b64d63c6bcb642e885916f3f8878d5221d9bd5
bertrammohr <bertram@mohr.nu>
2024-04-17 21:14:17 +0200

    mysql2 -> vercel postgres

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,57 +1,51 @@
-import mysql from 'mysql2';
+import { sql } from "@vercel/postgres";
 import { uuid } from 'uuidv4';
-import type { ConnectionOptions, RowDataPacket } from 'mysql2';
+
 import type { AchievedGoal } from '$lib/scheme';
-import type { StringifyOptions } from 'querystring';
-
-const access: ConnectionOptions = {
-    user: 'root',
-    database: 'weekly_goals',
-};
-
-const conn = mysql.createConnection(access);
 
 export const select = async (table: string, lastMondayFromOffset: string, nextSundayFromOffset: string) => {
     if (table != 'gym' && table != 'run' && table != 'core' && table != 'creatine') { return Promise.reject('Invalid table'); }
     
-    return new Promise<AchievedGoal[]>((resolve, reject) => {
-        conn.query<RowDataPacket[]>(`SELECT * FROM ${table} WHERE done_date BETWEEN ? AND ?;`, [lastMondayFromOffset, nextSundayFromOffset], (_err, rows) => {
-            if (_err) {
-                return reject(_err);
-            };
+    return new Promise<AchievedGoal[]>(async (resolve, reject) => {
+        try {
+            const { rows } = await sql`SELECT * FROM ${table} WHERE done_date BETWEEN ${lastMondayFromOffset} AND ${nextSundayFromOffset};`;
+
             resolve(rows.map((row) => {
                 return {
                     id: row.id,
                     date: row.done_date,
                 };
             }));
-        });
+        } catch (err) {
+            reject(err);
+        }
+
     });
 }
 
 export const addGoal = async (goal: string, submitDate: string) => {
-    return new Promise<string>((resolve, reject) => {
+    return new Promise<string>(async (resolve, reject) => {
         const id = uuid();
         if (goal != 'gym' && goal != 'run' && goal != 'core' && goal != 'creatine') { return Promise.reject('Invalid goal'); }
 
-        conn.query(`INSERT INTO ${goal} (id, done_date) VALUES (?, ?);`, [id, submitDate], (_err) => {
-            if (_err) {
-                return reject(_err);
-            };
+        try {
+            await sql`INSERT INTO ${goal} (id, done_date) VALUES (${id}, ${submitDate});`
             resolve(id);
-        });
+        } catch (err) {
+            reject(err);
+        }
     });
 }
 
 export const removeGoal = async (goal: string, submitDate: string) => {
-    return new Promise<string>((resolve, reject) => {
+    return new Promise<string>(async (resolve, reject) => {
         if (goal != 'gym' && goal != 'run' && goal != 'core' && goal != 'creatine') { return Promise.reject('Invalid goal'); }
 
-        conn.query(`DELETE FROM ${goal} WHERE done_date = ?;`, [submitDate], (_err) => {
-            if (_err) {
-                return reject(_err);
-            };
+        try {
+            await sql`DELETE FROM ${goal} WHERE done_date = ${submitDate};`
             resolve(submitDate);
-        });
+        } catch (err) {
+            reject(err);
+        }
     });
 }
