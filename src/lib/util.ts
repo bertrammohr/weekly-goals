c71220f134ccbf5e9e0451d79cc42e7c9ab4e298
bertrammohr <bertram@mohr.nu>
2024-05-01 10:51:46 +0200

    cleanup util

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -17,3 +17,29 @@ export const getNextSundayFromDate = (a: Date) => {
     d.setDate(d.getDate() - x + 6);
     return d;
 }
+
+export const getDayStringFromNumber = (day: number) => {
+    switch (day) {
+        case 0:
+            return "Søndag";
+        case 1:
+            return "Mandag";
+        case 2:
+            return "Tirsdag";
+        case 3:
+            return "Onsdag";
+        case 4:
+            return "Torsdag";
+        case 5:
+            return "Fredag";
+        case 6:
+            return "Lørdag";
+    }
+}
+
+export const getDateStringFromWeekday = (base: Date, day: number) => {
+    const newDate = new Date(base);
+    newDate.setDate(newDate.getDate() + day - 1 + (day == 0 ? 7 : 0));
+
+    return `${newDate.getDate() < 10 ? '0' + newDate.getDate() : newDate.getDate()}/${newDate.getMonth() + 1 < 10 ? '0' + (newDate.getMonth() + 1) : newDate.getMonth() + 1}`
+}
